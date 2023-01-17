0cdd0abc7ddae78ebf5d0e637f4f04cd0d59c9b1
Neal Chopra <nealchopra06@gmail.com>
2023-01-17 09:44:38 -0800

    added concise fn + updated readme

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-Meet Gorilla Guru, your personal opinion bot. Get insightful and thought-provoking opinions on any topic with the power of OpenAI. Simply type !opinion, !code, or !help followed by the topic on any Discord channel and Gorilla Guru will generate a response for you in no time. Perfect for stimulating conversation and seeking new perspectives.
+#Description
+Meet Gorilla Guru, your personal assistant bot. Get insightful and thought-provoking opinions on any topic with the power of OpenAI. Simply type the commands listed below followed by the topic on any Discord channel and Gorilla Guru will generate a response for you in no time. Perfect for stimulating conversation and seeking new perspectives.
 
-This bot was created using Python, and references the OpenAI API.
+#Instructions for use
+!help –– type !help see all possible commands
+!math –– type !math followed by any prompt to get the answer to your problem
+!opinion –– type !opinion followed by any prompt to get an opinion on a topic
+!code –– type !code and the language you'd like your program to be created in
+!translate –– type !translate followed by a word/phrase you'd like translated, then the language you want to translate to
+!concise –– type !concise followed by a prompt (can range in length) to make your writing more concise
--- discord_bot.py
+++ discord_bot.py
@@ -6,7 +6,15 @@ client = discord.Client()
 
 openai.api_key = config.OPENAI_API_KEY
 
+#!math
+async def calculate_math(message):
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=f"Calculate: {message}"
+    )
+    return response.choices[0].text
 
+#!opinion
 async def generate_opinion(message):
     response = openai.Completion.create(
         engine="text-davinci-002",
@@ -14,6 +22,7 @@ async def generate_opinion(message):
     )
     return response.choices[0].text
 
+#!code
 async def generate_code(language, message):
     response = openai.Completion.create(
         engine="text-davinci-002",
@@ -21,6 +30,7 @@ async def generate_code(language, message):
     )
     return response.choices[0].text
 
+#!translate
 async def translate_text(text, from_language, to_language):
     response = openai.Completion.create(
         engine="text-davinci-002",
@@ -28,23 +38,59 @@ async def translate_text(text, from_language, to_language):
     )
     return response.choices[0].text
 
+#!concise
+async def make_concise(text):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"Make this concise: {text}"
+    )
+    return response.choices[0].text
+
 @client.event
 async def on_message(message):
     try:
+        #!help
         if message.content == "!help":
             help_message = "Available commands: !opinion, !code <language> <prompt>, !translate <text> <from_language> <to_language> ,!help"
             await message.channel.send(help_message)
+        
+        #!math
+        elif message.content.startswith("!math"):
+            command, prompt = message.content.split(" ", 1)
+            response = openai.Completion.create(
+                engine="davinci",
+                prompt=f"Calculate: {prompt}"
+            )
+            answer = response.choices[0].text
+            await message.channel.send(answer)
+
+        #!opinion
         elif message.content.startswith("!opinion"):
             opinion = await generate_opinion(message.content[8:])
             await message.channel.send(opinion)
+
+        #!code
         elif message.content.startswith("!code"):
             command, language, prompt = message.content.split(" ", 2)
             code = await generate_code(language, prompt)
             await message.channel.send(code)
+
+        #!translate
         elif message.content.startswith("!translate"):
             command, text, from_language, to_language = message.content.split(" ", 3)
             translation = await translate_text(text, from_language, to_language)
             await message.channel.send(translation)
+        
+        #!concise
+        elif message.content.startswith("!concise"):
+            command, text = message.content.split(" ", 1)
+            response = openai.Completion.create(
+                engine="text-davinci-002",
+                prompt=f"Make this concise: {text}"
+            )
+            concise = response.choices[0].text
+            await message.channel.send(concise)
+
     except Exception as e:
         await message.channel.send("Sorry, something went wrong. Please try again later.")
         print(e)
